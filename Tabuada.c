2579b8f614d5eedbf0a1dc77f68de81f2e3572e0
sabrina silva <102616948+sabrina702@users.noreply.github.com>
2022-06-02 18:38:44 -0300

    Update Tabuada.c

--- Tabuada.c
+++ Tabuada.c
@@ -4,9 +4,8 @@ int main(){
 	int num,cont,mult;
 	scanf("%d",&num);
 	for(cont=1;cont<=1000;cont++){
-		cont++;
 		mult=cont*num;
+		printf("%d x %d = %d",cont,num,mult);
 	}
-	printf("%d x %d = %d",cont,num,mult);
 	return 0;
 }
